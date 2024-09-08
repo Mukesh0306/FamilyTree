23b52c9fbe8298427b23545fe6ce43c303b6d7a0
Mukesh0306 <mukesh.ramanna12@gmail.com>
2024-09-08 14:49:57 +0530

    Css modification

--- src/Nav.js
+++ src/Nav.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import './Nav.css';  // Import the custom CSS
+import './Nav.css';
 
 function NavComponent() {
   return (
-    <div>
-      <nav className="nav-component">
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/parents">Parents</Link></li>
-          <li><Link to="/manjunath">Manjunath</Link></li>
-          <li><Link to="/mukesh">Mukesh</Link></li>
-          <li><Link to="/praveen">Praveen</Link></li>
-        </ul>
-      </nav>
+    <div className="nav-component">
+      <button className="home-button">Home</button>
+      <div className="scrolling-text"></div>
     </div>
   );
 }
