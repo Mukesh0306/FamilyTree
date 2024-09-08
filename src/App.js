4252a36b9086fd9829b17094fa624e1b2b12fc88
Mukesh0306 <mukesh.ramanna12@gmail.com>
2024-09-08 14:58:51 +0530

    css update in app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,38 +1,30 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
-import NavComponent from './Nav';  // Import the NavComponent
-import TreeBranches from './TreeBranches'; // Import the TreeBranches component
-import SpeechRecog from './SpeechRecog';  // Import the SpeechRecog component
-import Manjunath from './Manjunath';  // Import the Manjunath component
-import Mukesh from './Mukesh';  // Import the Mukesh component
-import Praveen from './Praveen';  // Import the Praveen component
-import Parents from './Parents';  // Import the Parents component
-import './App.css'; // Import your custom CSS
+import NavComponent from './Nav';
+import TreeBranches from './TreeBranches';
+import SpeechRecog from './SpeechRecog';
+import Manjunath from './Manjunath';
+import Mukesh from './Mukesh';
+import Praveen from './Praveen';
+import Parents from './Parents';
+import './App.css';
 
 function App() {
-  const location = useLocation();  // Hook to access the current route
+  const location = useLocation();
 
-  // Check if the current route is one of the specific pages
   const isSpecificPage = location.pathname === "/manjunath" || location.pathname === "/mukesh" || location.pathname === "/praveen" || location.pathname === "/parents";
 
   return (
     <div className="app-container">
-      {/* Navigation Bar */}
       <NavComponent />
 
-      {/* Main Content */}
       {!isSpecificPage && (
         <div className="main-content">
-          <div className="left-side">
-            <TreeBranches />
-          </div>
-          <div className="right-side">
-            <SpeechRecog />
-          </div>
+          <TreeBranches />
+          <SpeechRecog />
         </div>
       )}
 
-      {/* Routes for each branch */}
       <Routes>
         <Route path="/parents" element={<Parents />} />
         <Route path="/manjunath" element={<Manjunath />} />
